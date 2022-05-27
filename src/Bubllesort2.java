9879a50d41241c555ee4a2a8f481d5257aa22102
avrians <avriansyahb10@gmail.com>
2022-05-27 22:54:53 +0700

    ver02

---
+++ src/Bubllesort2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Bubllesort2
+{
+    private static void sorting(int[] dataKe) {
+        int jumlah = 5;
+        for(int i=0; i<(jumlah-1);i++)
+        {
+            for(int j=0;j<jumlah-i-1;j++)
+            {
+                if (dataKe[j] > dataKe[j+1])
+                {
+                    int swap = dataKe[j];
+                    dataKe[j] = dataKe[j+1];
+                    dataKe[j+1]=swap;
+                }
+            }
+        }
+        System.out.println("\n\nBilangan Terurutnya adalah :");
+        for(int i=0;i<jumlah; i++)
+        System.out.print(dataKe[i] +" ");
+        System.out.println("");
+    }
+    public static void main(String[]args)
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Masukkan Jumlah Bilangan Yang Mau DiInputkan :");
+        int jumlah = 5;
+        
+        int array[] = new int[jumlah];
+        
+        System.out.println("\nMasukkan " + jumlah+" Buah Bilangan Integer");
+        System.out.println("===========================================");
+        int nomor = 0;
+        for(int i=0; i<jumlah;i++)
+        {
+            nomor++;
+            System.out.print("Bilangan Ke- " + nomor+" =");
+            array[i]=scan.nextInt();
+            sorting(array);
+        }
+        
+    }
+}
